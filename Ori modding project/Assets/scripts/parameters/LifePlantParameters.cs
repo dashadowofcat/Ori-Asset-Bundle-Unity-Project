501a68db9050ef639189d106439ec26e8fbc505f
BenjaminNelson <s-benjamin.nelson@lwtech.edu>
2024-07-29 14:30:52 -0400

    Implmented Life Plant and added sprites for it and springs.

---
+++ Ori modding project/Assets/scripts/parameters/LifePlantParameters.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[ExecuteInEditMode]
+
+public class LifePlantParameters : ElementParameter
+{
+    [InfoBox("The max amount of orbs created based on current health", EInfoBoxType.Normal)]
+    public int IdealOrbs;
+    [InfoBox("Will overwrite dynamic orb scaling based on current health if > 0", EInfoBoxType.Normal)]
+    public int NumberOfHealthOrbs;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        SetInt("IdealOrbs", IdealOrbs);
+        SetInt("NumberOfHealthOrbs", NumberOfHealthOrbs);
+
+    }
+}
