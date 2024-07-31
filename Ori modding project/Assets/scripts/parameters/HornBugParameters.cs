68fca50a1858a89c489cd2e2b3c20af401048987
BenjaminNelson <s-benjamin.nelson@lwtech.edu>
2024-07-31 19:03:25 -0400

    Began implementing Horn Bug

---
+++ Ori modding project/Assets/scripts/parameters/HornBugParameters.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[ExecuteInEditMode]
+
+public class HornBugParameter : ElementParameter
+{
+    [InfoBox("The health of the enemy", EInfoBoxType.Normal)]
+    public int Health;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetInt("Health", Health);
+    }
+}
