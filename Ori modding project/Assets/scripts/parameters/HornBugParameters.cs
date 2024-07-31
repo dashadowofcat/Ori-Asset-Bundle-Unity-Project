eecd497a7d136d035e32889171e6149e3e229fc2
BenjaminNelson <s-benjamin.nelson@lwtech.edu>
2024-07-31 19:42:50 -0400

    More Horn Bug Experimentation

--- Ori modding project/Assets/scripts/parameters/HornBugParameters.cs
+++ Ori modding project/Assets/scripts/parameters/HornBugParameters.cs
@@ -7,12 +7,5 @@ using NaughtyAttributes;
 
 public class HornBugParameter : ElementParameter
 {
-    [InfoBox("The health of the enemy", EInfoBoxType.Normal)]
-    public int Health;
-
-    // Update is called once per frame
-    void Update()
-    {
-        SetInt("Health", Health);
-    }
+   
 }
