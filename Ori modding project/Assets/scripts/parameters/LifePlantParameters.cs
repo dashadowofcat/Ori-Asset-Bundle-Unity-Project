b0e9984da952c615fddbc2fc7e466e958e5ed5d8
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-08-03 21:23:35 +0300

    Reapply "added element icons, hornbug and mantis paramters"
    This reverts commit fa04bee770203d0b7e8f580721267ad5e0d81045.

--- Ori modding project/Assets/scripts/parameters/LifePlantParameters.cs
+++ Ori modding project/Assets/scripts/parameters/LifePlantParameters.cs
@@ -7,9 +7,8 @@ using NaughtyAttributes;
 
 public class LifePlantParameters : ElementParameter
 {
-    [InfoBox("The max amount of orbs created based on current health", EInfoBoxType.Normal)]
     public int IdealOrbs;
-    [InfoBox("Will overwrite dynamic orb scaling based on current health if > 0", EInfoBoxType.Normal)]
+    [InfoBox("Will overwrite dynamic orb scaling if larger than one", EInfoBoxType.Normal)]
     public int NumberOfHealthOrbs;
     
     // Update is called once per frame
