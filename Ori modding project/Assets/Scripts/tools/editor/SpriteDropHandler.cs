7383d4c7ec768088aa552f6374b1f58f7e6c8c47
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-08-21 22:55:12 +0300

    added new sprite conversion system

---
+++ Ori modding project/Assets/Scripts/tools/editor/SpriteDropHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class SpriteDropHandler : MonoBehaviour
+{
+    static SpriteDropHandler()
+    {
+        SceneView.onSceneGUIDelegate += OnSceneGUI;
+    }
+
+    private static void OnSceneGUI(SceneView sceneView)
+    {
+        Event Event = Event.current;
+
+        if (Event.type == EventType.DragPerform)
+        {
+            DragAndDrop.AcceptDrag();
+
+            foreach (Object draggedObject in DragAndDrop.objectReferences)
+            {
+                if (draggedObject is Sprite sprite)
+                {
+
+                    Vector3 mousePosition = HandleUtility.GUIPointToWorldRay(Event.mousePosition).origin;
+
+                    ConvertTo3DPlane(sprite.texture, mousePosition);
+                }
+            }
+
+            Event.Use();
+        }
+    }
+
+    private static void ConvertTo3DPlane(Texture2D texture, Vector3 position)
+    {
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        Renderer renderer = plane.GetComponent<Renderer>();
+
+        DestroyImmediate(plane.GetComponent<MeshCollider>());
+
+        Material spriteMaterial = new Material(Shader.Find("Sprites/Default"));
+        spriteMaterial.mainTexture = texture;
+
+        string materialFolderPath = "Assets/level data/cache/materials";
+        string materialPath = $"{materialFolderPath}/{texture.name} ({plane.GetInstanceID()})_Material.mat";
+        AssetDatabase.CreateAsset(spriteMaterial, materialPath);
+        AssetDatabase.SaveAssets();
+
+        renderer.sharedMaterial = spriteMaterial;
+
+        position.z = 0;
+
+        plane.transform.position = position;
+
+        plane.transform.localScale = new Vector3(texture.width / 1000f, 1, texture.height / 1000f);
+        plane.transform.eulerAngles = new Vector3(90, 0, 180);
+
+        plane.name = texture.name;
+
+        Selection.activeGameObject = plane;
+    }
+}
