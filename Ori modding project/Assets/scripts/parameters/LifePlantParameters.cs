0c8cd342f8d5b318c6fbdbcabf5a4d059eeea273
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-12-08 15:22:46 +0200

    minor changes

--- Ori modding project/Assets/scripts/parameters/LifePlantParameters.cs
+++ Ori modding project/Assets/scripts/parameters/LifePlantParameters.cs
@@ -6,14 +6,11 @@ using NaughtyAttributes;
 [ExecuteInEditMode]
 public class LifePlantParameters : ElementParameter
 {
-    public int IdealOrbs;
-    [InfoBox("Will overwrite dynamic orb scaling if larger than one", EInfoBoxType.Normal)]
     public int NumberOfHealthOrbs;
     
     // Update is called once per frame
     void Update()
     {
-        SetInt("IdealOrbs", IdealOrbs);
         SetInt("NumberOfHealthOrbs", NumberOfHealthOrbs);
 
     }
