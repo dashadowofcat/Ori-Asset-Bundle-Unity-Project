16077df8a993ac944d26490e0d99380a7b04b131
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2025-03-27 21:56:59 +0200

    added level titles

---
+++ Ori modding project/Assets/Scripts/parameters/LevelTitleParemeters.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LevelTitleParemeters : ElementParameter
+{
+    public string Title;
+
+    void Update()
+    {
+        SetString("Title", Title);
+    }
+}
