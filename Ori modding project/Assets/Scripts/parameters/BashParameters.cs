784437eced5fa9b8e1ffe068eeaa786ef62706ab
CompyPro <compro510@gmail.com>
2025-08-24 00:03:12 -0400

    Added bash and music parameters

---
+++ Ori modding project/Assets/Scripts/parameters/BashParameters.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BashParameters : ElementParameter
+{
+    public bool hasParticles;
+
+    void Update()
+    {
+        SetBool("HasParticles", hasParticles);
+    }
+}
---
+++ Ori modding project/Assets/Scripts/parameters/MusicParameters.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MusicParameters : ElementParameter
+{
+    public InGameMusic inGameMusic;
+
+    void Update()
+    {
+        SetString("InGame", Enum.GetName(typeof(InGameMusic), inGameMusic));
+    }
+
+    public enum InGameMusic
+    {
+        None,
+        WellspringGlades,
+        LumaPools
+    }
+}
